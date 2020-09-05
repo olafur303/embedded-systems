a87599e56d74684e9a1f387c9df2feb99b8fe2db
xyderos <kosxyderos@gmail.com>
2020-09-05 17:09:53 +0200

    finished the produxer consumer with the rendezvous technique

--- redenzvous/producerconsumer_rndzvs.adb
+++ redenzvous/producerconsumer_rndzvs.adb
@@ -1,124 +1,148 @@
-with Ada.Text_IO;
-use Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
 
-with Ada.Real_Time;
-use Ada.Real_Time;
+with Ada.Real_Time; use Ada.Real_Time;
 
 with Ada.Numerics.Discrete_Random;
 
 procedure ProducerConsumer_Rndzvs is
-	
+
    N : constant Integer := 10; -- Number of produced and consumed tokens per task
-	
-   X : constant Integer := 3; -- Number of producers and consumers	
-	
+
+   X : constant Integer := 3; -- Number of producers and consumers
+
    -- Random Delays
-   subtype Delay_Interval is Integer range 50..250;
-   
+   subtype Delay_Interval is Integer range 50 .. 250;
+
    package Random_Delay is new Ada.Numerics.Discrete_Random (Delay_Interval);
-  
+
    use Random_Delay;
-  
+
    G : Generator;
 
    task Buffer is
-  
-      entry Append(I : in Integer);
-  
-      entry Take(I : out Integer);
-  
-   end Buffer;
 
-   bfr : Buffer;
+      entry Append(I : in Integer);
+
+      entry Take (I : out Integer);
+
+   end Buffer;
 
    task type Producer;
 
    task type Consumer;
-   
+
    task body Buffer is
-         
-         Size: constant Integer := 4;
-         
-         type Index is mod Size;
-         
-         type Item_Array is array(Index) of Integer;
-         
-         B : Item_Array;
-         
-         In_Ptr, Out_Ptr, Count : Index := 0;
-   
+
+      Size : constant Integer := 4;
+
+      type Index is mod Size;
+
+      type Item_Array is array (Index) of Integer;
+
+      B : Item_Array;
+
+      In_Ptr, Out_Ptr, Count : Index := 0;
+
    begin
-   
+
       loop
-   
-         select when Count < N =>
 
-            accept Append(value : in Integer) do
-            
-            A(In_Ptr):= value;
-            
-            In_Ptr:= In_Ptr+1;
-            
-            Count:= Count+1;
-            
-            Put_Line("Producer: Putting "&value'Img & "into the buffer");
-            
-            end Put;
+         select 
+            when Integer(Count) < N =>
 
-         or when Count > 0 =>
-				
-            accept Take(value : out Integer) do
-            
-            value := A(Out_Ptr);
+               accept Append(I : in Integer) do
 
-            Out_Ptr:= Out_Ptr -1;
-            
-            Count := Count - 1;
+                  B(In_Ptr) := I;
 
-            end Take;
+                  In_Ptr := In_Ptr + 1;
+
+                  Count := Count + 1;
+
+                  Put_Line ("Message from Buffer : Putting " & I'Img & " into the buffer.");
+
+               end Append;
+
+         or 
+         
+            when Count > 0 =>
+
+               accept Take (I : out Integer) do
+
+                  I := B(Out_Ptr);
+
+                  Out_Ptr := Out_Ptr - 1;
+
+                  Count := Count - 1;
+
+                  Put_Line ("Message from the buffer : Taking " & I'Img &  " from the buffer.");
+
+               end Take;
          or
-				-- => Termination
+            
+            terminate;
+         
          end select;
-      end loop;
-   end Buffer;
       
-   task body Producer is
-      Next : Time;
-   begin
-      Next := Clock;
-      for I in 1..N loop
-			
-         -- => Complete code: Write to X
+      end loop;
+   
+   end Buffer;
 
-         bfr.Append(X);
+   task body Producer is
+      
+      Next : Time;
+
+      Data : Integer;
+
+   begin
+      
+      Next := Clock;
+
+      for I in 1 .. N loop
+
+         Data := Random(G);
+
+         buffer.Append(Data);
+
+         Put_Line("Message form the producer: Putting : " & Data'Img & " into the buffer.");
 
          -- Next 'Release' in 50..250ms
-         Next := Next + Milliseconds(Random(G));
+         
+         Next := Next + Milliseconds (Random (G));
+         
          delay until Next;
+      
       end loop;
-   end;
+   
+   end Producer;
 
    task body Consumer is
+     
       Next : Time;
-      X : Integer;
+    
+      Data : Integer;
+  
    begin
+   
       Next := Clock;
-      for I in 1..N loop
-         -- Complete Code: Read from X
+   
+      for I in 1 .. N loop
+      
+         buffer.Take(Data);
 
-         brf.Take(X);
-
-         Put_Line(Integer'Image(X));
-         Next := Next + Milliseconds(Random(G));
+         Put_Line("Message form the Consumer: Taking : "& Data'Img & " from the buffer.");
+        
+         Next := Next + Milliseconds (Random (G));
+        
          delay until Next;
+      
       end loop;
-   end;
-	
-	P: array (Integer range 1..X) of Producer;
-	C: array (Integer range 1..X) of Consumer;
-	
+   
+   end Consumer;
+
+   P : array (Integer range 1 .. X) of Producer;
+  
+   C : array (Integer range 1 .. X) of Consumer;
+
 begin -- main task
    null;
 end ProducerConsumer_Rndzvs;
-
-
